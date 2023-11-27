097576a2a963d8dbbc901758d6dd02445171e68d
kinger83 <kinger83@outlook.com>
2023-11-27 13:24:28 +1100

    polylines working

--- Project 9 - Phone App/phone_app/lib/gps.dart
+++ Project 9 - Phone App/phone_app/lib/gps.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({super.key});
@@ -20,15 +21,34 @@ class _MapPageState extends State<MapPage> {
       Completer<GoogleMapController>();
   Location _locationController = new Location();
   LatLng? _currentPosition = null;
-  bool _isTracking = false;
+  bool _isTracking = true;
+
+  List<List> _coordsList = [];
+  List<LatLng> _polylineCoords = [];
+  Polyline _polyLine = Polyline(
+    polylineId: PolylineId("userRoute"),
+    color: Colors.blue,
+    width: 5,
+    points: [],
+  );
+
+  // test variables
+  static const LatLng _initPos = LatLng(-38.1986127, 144.2986353);
+  static const LatLng _endPos = LatLng(-38.1990677, 144.3081407);
 
   @override
   void initState() {
     super.initState();
     setLocation();
-    //Timer.periodic(Duration(seconds: 1), (timer) {
+    //Timer.periodic(Duration(seconds: 10), (timer) {
     //getLocationUpdates();
     //});
+  }
+
+  @override
+  void dispose() {
+    //_mapController.dispose();
+    super.dispose();
   }
 
   @override
@@ -42,7 +62,7 @@ class _MapPageState extends State<MapPage> {
               onMapCreated: ((GoogleMapController controller) =>
                   _mapController.complete(controller)),
               initialCameraPosition: CameraPosition(
-                target: _currentPosition!,
+                target: _initPos,
                 zoom: 15,
               ),
               markers: {
@@ -55,7 +75,7 @@ class _MapPageState extends State<MapPage> {
                     icon: BitmapDescriptor.defaultMarker,
                     position: _currentPosition!)
               },
-              //TODO Make start pos set when user clicks begi workout.
+              polylines: {_polyLine},
             ),
     );
   }
@@ -97,12 +117,29 @@ class _MapPageState extends State<MapPage> {
     //     .listen((LocationData currentLocation) {
 
     LocationData currentLocation = await _locationController.getLocation();
+
     if (currentLocation.latitude != null && currentLocation.longitude != null) {
       setState(() {
         _currentPosition =
-            LatLng(currentLocation.latitude!, currentLocation.longitude!);
+            //TODO change this before prduction
+            // actual code
+            //LatLng(currentLocation.latitude!, currentLocation.longitude!);
+
+            // test code
+            _initPos;
       });
+
+      // Add polyline co-ord
+      _polylineCoords.add(_currentPosition!);
       cameraFollow(_currentPosition!);
+      int timercount = 0;
+      Timer.periodic(Duration(seconds: 10), (timer) {
+        getLocationUpdates();
+        timercount++;
+        if (timercount >= 1) {
+          timer.cancel();
+        }
+      });
     }
 
     //});
@@ -133,18 +170,47 @@ class _MapPageState extends State<MapPage> {
     // _locationController.onLocationChanged
     //     .listen((LocationData currentLocation) {
     if (_isTracking) {
+      //**********Actual code
+      //TODO use this for production */
+      // LocationData currentLocation = await _locationController.getLocation();
+      // if (currentLocation.latitude != null &&
+      //     currentLocation.longitude != null) {
+      //   setState(() {
+      //     _currentPosition =
+      //         LatLng(currentLocation.latitude!, currentLocation.longitude!);
+
       LocationData currentLocation = await _locationController.getLocation();
       if (currentLocation.latitude != null &&
           currentLocation.longitude != null) {
         setState(() {
-          _currentPosition =
-              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _currentPosition = _endPos;
         });
+
+        // Add polyline co-ord
+        _polylineCoords.add(_currentPosition!);
+        // update polylines
+        _polyLine = Polyline(
+            polylineId: PolylineId("userRoute"),
+            color: Colors.blue,
+            width: 5,
+            points: _polylineCoords);
+
+        // camera to follow
         cameraFollow(_currentPosition!);
       }
     }
     //});
   } // end getLocationUpdates
+
+  // **** poly line and waypoints
+
+  // ******* Logging functions
+
+  double? distanceCovered = null;
+  double? workOutSeconds = null;
+  double? evelvationChange = null;
+  double? avgSpeed = null;
+  double? topSpeed = null;
 }// end class
 
 
